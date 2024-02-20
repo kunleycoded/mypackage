edc98d9bed9d94f099cb07352175f1b0d4735ef2
Adekunle Okunade <adekunlediamond244@gmail.com>
2024-02-20 13:49:53 +0100

    Adding files and packages

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/kunleycoded/package',
+    author='Addekunle',
+    author_email='adekunlediamond244@gmail.com'
+)
